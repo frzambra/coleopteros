eaa68979a85793adaf075f86cecf40fe0b0aa1c2
francisco <frzambra@gmail.com>
2023-11-04 13:34:43 -0300

    agregar reporte modelo distribucion frickius v1.0

--- scripts/01_analisis_preliminar.R
+++ scripts/01_analisis_preliminar.R
@@ -17,6 +17,66 @@ sm <- rast(dir_ls(dir_sm,regexp = '(2017|2018|2019|2020|2021|2022).*tif$'))
 temp <- rast(dir_ls(dir_temp,regexp = '(2017|2018|2019|2020|2021|2022).*tif$'))
 pre <- rast(dir_ls(dir_pre,regexp = '(2017|2018|2019|2020|2021|2022).*tif$'))
 
+# crear sumatoria humedad de suelo
+zone <- st_bbox(data) |> st_as_sfc() |> st_as_sf() |> st_buffer(10000)
+
+ind <- lapply(1:6,\(x) rep(x,12)) |> unlist()
+
+sm_z <- crop(sm,zone)
+csm_z <- tapp(sm_z,ind,sum)
+resam <- sm_z
+csm_z_res <- resample(csm_z,disagg(sm_z,20))
+
+#mapa de humedad acumulada anual con la ubicación de los puntos
+
+map_hs <- tm_shape(mean(csm_z_res)) + 
+  tm_raster(style = 'cont',title ='Annual Soil Moisture (mm)',palette = 'Blues') + 
+  tm_shape(data) + 
+  tm_dots(col = 'Presence',style = 'cat',labels = c('No','Yes'),palette = c('blue','red')) +
+  #tm_graticules() +
+  tm_compass(position = c('right','top')) +
+  tm_scale_bar() +
+  tm_layout(legend.outside = TRUE)
+tmap_save(map_hs,'output/figs/mapa_humedad_suelo_anual.png',scale = 1)
+
+# crear sumatoria precipitación
+
+pre_z <- crop(pre,zone)*1000
+pre_z <- tapp(pre_z,ind,sum)
+resam <- pre_z
+pre_z_res <- resample(pre_z,disagg(sm_z,20))
+
+#mapa de humedad acumulada anual con la ubicación de los puntos
+
+map_pre <- tm_shape(mean(pre_z_res)) + 
+  tm_raster(style = 'cont',title ='Annual Precipitation (mm)',palette = 'Blues') + 
+  tm_shape(data) + 
+  tm_dots(col = 'Presence',style = 'cat',labels = c('No','Yes'),palette = c('blue','red')) +
+  #tm_graticules() +
+  tm_compass(position = c('right','top')) +
+  tm_scale_bar() +
+  tm_layout(legend.outside = TRUE)
+tmap_save(map_pre,'output/figs/mapa_precipitacion_anual.png',scale = 1)
+
+## Mapa temperatura promedio
+temp_z <- crop(temp,zone)-273.15
+temp_z <- tapp(temp_z,ind,mean)
+resam <- temp_z
+temp_z_res <- resample(temp_z,disagg(sm_z,20))
+
+#mapa de humedad acumulada anual con la ubicación de los puntos
+
+map_temp <- tm_shape(mean(temp_z_res)) + 
+  tm_raster(style = 'cont',title ='Mean Annual Temperature (°C)',palette = 'Reds',alpha = .6) + 
+  tm_shape(data) + 
+  tm_dots(col = 'Presence',style = 'cat',labels = c('No','Yes'),palette = c('blue','red')) +
+  #tm_graticules() +
+  tm_compass(position = c('right','top')) +
+  tm_scale_bar() +
+  tm_layout(legend.outside = TRUE)
+tmap_save(map_temp,'output/figs/mapa_temperatura_anual.png',scale = 1)
+
+###
 df_sm <- extract(sm,data) |> cbind(data)
 names(df_sm)[2:73] <-  str_extract(names(df_sm)[2:73],'[0-9]{4}-[0-9]{2}-[0-9]{2}')
 df_sm |> 
@@ -30,6 +90,20 @@ df_sm |>
   theme_bw()
 ggsave('output/figs/variacion_sm_coleopteros.png',scale=2)
 
+map_sm <- df_sm |> 
+  pivot_longer(2:73) |> 
+  mutate(dates = ymd(name)) |> 
+  select(-name) |> 
+  group_by(Sector,geometry) |> 
+  summarize(med = median(value,na.rm=TRUE)) |> 
+  st_as_sf() |> 
+  tm_shape() + 
+  tm_dots(col="med",style='jenks')
+tmap_save(map_sm,'output/figs/map_sm_median_2017-2023.html')
+
+ggplot(aes(lon,med,color=Sector))+ 
+  geom_point()
+  
 df_sm |> 
   pivot_longer(2:73) |> 
   mutate(dates = ymd(name)) |> 
@@ -43,6 +117,8 @@ ggsave('output/figs/boxplot_sm_coleopteros.png',scale=2)
 
 df_temp <- extract(temp,data) |> cbind(data)
 names(df_temp)[2:73] <-  str_extract(names(df_temp)[2:73],'[0-9]{4}-[0-9]{2}-[0-9]{2}')
+
+
 df_temp |> 
   pivot_longer(2:73) |> 
   mutate(dates = ymd(name)) |> 
@@ -69,6 +145,18 @@ ggsave('output/figs/boxplot_temp_coleopteros.png',scale=2)
 
 df_pre <- extract(pre,data) |> cbind(data)
 names(df_pre)[2:73] <-  str_extract(names(df_pre)[2:73],'[0-9]{4}-[0-9]{2}-[0-9]{2}')
+
+map_pre <- df_pre |> 
+  pivot_longer(2:73) |> 
+  mutate(dates = ymd(name)) |> 
+  select(-name) |> 
+  group_by(Sector,geometry) |> 
+  summarize(med = median(value,na.rm=TRUE)) |> 
+  st_as_sf() |> 
+  tm_shape() + 
+  tm_dots(col="med",style='jenks')
+tmap_save(map_pre,'output/figs/map_pre_median_2017-2023.html')
+
 df_pre |> 
   pivot_longer(2:73) |> 
   mutate(dates = ymd(name)) |> 
---
+++ scripts/02_model_random_forest.R
@@ -0,0 +1,226 @@
+library(tidyverse)
+library(tidymodels)
+library(sf)
+library(tmap)
+library(fs)
+library(terra)
+library(rnaturalearth)
+
+## leer los datos
+data <-  readxl::read_xlsx('data/raw/Frickius_iNaturalist_BahiaExploradores.xlsx',sheet=1) |> 
+  st_as_sf(coords =c('longitude','latitude'),crs=4326) 
+
+## agregar coordenadas
+data <- data |> cbind(lon=st_coordinates(data)[,1])
+
+#extensión de Chile
+chl <- ne_countries(country = 'Chile',scale = 'medium',returnclass = 'sf')
+
+#pseudo ausencia de la especie
+sa <- st_as_sfc(st_bbox(data)) |> st_sample(100) 
+
+# extensión espacial que se considerará para el modelo
+bb <- st_bbox(c(xmin=-77,ymin=-56,xmax=-63,ymax=-37))
+
+#se agregan los puntos de seudo-ausencia de la especie
+data4model <- c(data$geometry,sa) |> st_as_sf()
+
+# 1= presencia de la especie, 0 = seudo-ausencia de la especie
+data4model$pres <- NA
+data4model$pres[1:49] <- 1
+data4model$pres[50:149] <- 0
+
+#predictores rasters humedad de suelo (sm), temperatura (temp) y precipitación (temp)
+dir_sm <- '/mnt/md0/raster_procesada/ERA5-Land_tiff/clima/volumetric_soil_water/monthly'
+dir_temp <-  '/mnt/md0/raster_procesada/ERA5-Land_tiff/clima/2m_mean_temperature/monthly'
+dir_pre <-  '/mnt/md0/raster_procesada/ERA5-Land_tiff/clima/total_precipitation/monthly'
+
+sm <- rast(dir_ls(dir_sm,regexp = '(2017|2018|2019|2020|2021|2022).*tif$'))
+temp <- rast(dir_ls(dir_temp,regexp = '(2017|2018|2019|2020|2021|2022).*tif$'))
+pre <- rast(dir_ls(dir_pre,regexp = '(2017|2018|2019|2020|2021|2022).*tif$'))
+
+#indices para calcular los promedios mensuales entre 2017-2022
+ind <- rep(1:12,6)
+
+#predictores agregados mensuales
+sm_z <- crop(sm,bb)
+csm_z <- tapp(sm_z,ind,sum)
+names(csm_z) <- paste0('HS_',month.abb)
+
+pre_z <- crop(pre,bb)*1000
+pre_z <- tapp(pre_z,ind,sum)
+names(pre_z) <- paste0('Pre_',month.abb)
+  
+temp_z <- crop(temp,bb)-273.15
+temp_z <- tapp(temp_z,ind,mean)
+names(temp_z) <- paste0('Temp_',month.abb)
+
+#NDVI índice de vegetación
+dir_ndvi <- '/mnt/md0/raster_procesada/MODIS/NDVI.MOD13A3.061'
+files_ndvi <- dir_ls(dir_ndvi,regexp = 'tif$')
+ind <- sapply(1:12,\(i) seq(i,284,12))
+res <- lapply(ind,\(i){
+  rast(files_ndvi[i]) |> app(mean)
+})
+
+ndvi_mes <- rast(res)
+ndvi_mes_r <- project(ndvi_mes,temp_z)
+ndvi_mes_r <- crop(ndvi_mes_r,bb)
+names(ndvi_mes_r) <- paste0('ndvi_',month.abb)
+
+#resampleo para que los predictores tengan la misma resolución espacial
+sm_z <-  resample(sm_z,ndvi_mes_r[[1]])
+temp_z <- resample(temp_z,ndvi_mes_r[[1]])
+pre_z <- resample(pre_z,ndvi_mes_r[[1]])
+
+#elevación
+library(geodata)
+#obtener elevación a 30" de resolución espacial
+dem <- elevation_30s('chile', path=tempdir())
+dem <- crop(dem,bb)
+dem <- resample(dem,ndvi_mes_r[[1]])
+names(dem) <- 'dem'
+
+#unir todos los predictores
+preds <- c(ndvi_mes_r,pre_z,temp_z,dem)
+
+data4model <- cbind(data4model,extract(preds,data4model))
+
+library(tidyverse)
+library(tidymodels)
+
+#Preparar los datos para la modelación
+data4modelf <- data4model |> 
+  st_drop_geometry() |> 
+  select(-ID) |> 
+  mutate(across(-pres,scale)) |> 
+  mutate(pres = factor(pres)) |> 
+  drop_na()
+
+set.seed(123)
+
+#dividir set de datos en entrenamiento y test
+fric_split <- initial_split(data4modelf,strata = pres)
+
+fric_split_train <- training(fric_split)
+fric_test <- testing(fric_split)
+fric_train <- training(fric_split)
+
+#modelo random forest 
+rf_spec <- rand_forest(trees = 1000, mode = "classification") |> 
+  set_args(importance = 'impurity')
+rf_wflow <- workflow(pres ~ ., rf_spec)
+rf_fit <- fit(rf_wflow, fric_train)
+
+# modelo naibe bayes
+nb_spec <- naive_Bayes(mode = "classification")
+nb_wflow <- workflow(pres ~ ., nb_spec)
+nb_fit <- fit(nb_wflow, fric_train)
+
+augment(rf_fit, new_data = fric_train) %>%
+  conf_mat(truth = pres, estimate = .pred_class)
+
+augment(rf_fit, new_data = fric_train) %>%
+  conf_mat(truth = pres, estimate = .pred_class) %>%
+  autoplot(type = "heatmap")
+
+augment(rf_fit, new_data = fric_test) %>%
+  accuracy(truth = pres, estimate = .pred_class)
+
+
+preds_tbl <- values(preds) |> 
+  as_tibble() |> 
+  mutate(across(everything(),\(x) scale(x)[,1]))
+
+preds_tbl |> 
+  rowid_to_column() |> 
+  filter(if_all(everything(),\(x) !is.na(x))) |> 
+  pull(rowid) -> rid
+
+preds_fric_rf <- predict(rf_fit,preds_tbl[rid,])
+preds_fric_nb <- predict(nb_fit,preds_tbl[rid,])
+
+preds_final_rf <- tibble(preds = rep(NA,nrow(preds_tbl)))
+preds_final_nb <- tibble(preds = rep(NA,nrow(preds_tbl)))
+
+preds_final_rf[rid,] <- preds_fric_rf$.pred_class
+preds_final_nb[rid,] <- preds_fric_nb$.pred_class
+
+res_rf <- preds[[1]]
+res_nb <- preds[[1]]
+values(res_rf) <- NA 
+values(res_nb) <- NA 
+
+values(res_rf) <- preds_final_rf$preds
+values(res_nb) <- preds_final_nb$preds
+plot(res_rf)
+plot(res_nb)
+#
+#
+library(vip)
+rf_fit |> 
+extract_fit_parsnip() |> 
+vip(geom = "point") + 
+  labs(title = "Random forest variable importance") 
+
+nb_fit |> 
+  extract_fit_parsnip() |> 
+  vip(geom = "point") + 
+  labs(title = "Random forest variable importance") 
+
+# Model Evaluation
+
+set.seed(345)
+folds <- vfold_cv(fric_split_train, v = 5)
+folds
+
+rf_wf <- 
+  workflow() |> 
+  add_model(rf_spec) |> 
+  add_formula(pres ~ .)
+
+nb_wf <- 
+  workflow() |> 
+  add_model(nb_spec) |> 
+  add_formula(pres ~ .)
+
+set.seed(456)
+rf_fit_rs <- 
+  rf_wf |> 
+  fit_resamples(folds)
+
+nb_fit_rs <- 
+  nb_wf |> 
+  fit_resamples(folds)
+
+rf_fit_rs |> 
+  unnest(c(.metrics))
+
+nb_fit_rs |> 
+  unnest(c(.metrics))
+
+collect_metrics(rf_fit_rs)
+
+rf_testing_pred <- 
+  predict(rf_fit, fric_test) |> 
+  bind_cols(predict(rf_fit, fric_test, type = "prob")) %>% 
+  bind_cols(fric_test %>% dplyr::select(pres))
+
+nb_testing_pred <- 
+  predict(nb_fit, fric_test) |> 
+  bind_cols(predict(nb_fit, fric_test, type = "prob")) %>% 
+  bind_cols(fric_test %>% dplyr::select(pres))
+
+rf_testing_pred |>                    # test set predictions
+  #mutate(across(everything(),as.numeric)) |> 
+  roc_auc(truth = pres, .pred_0)
+
+nb_testing_pred |>                    # test set predictions
+  #mutate(across(everything(),as.numeric)) |> 
+  roc_auc(truth = pres, .pred_0)
+
+rf_testing_pred %>%                   # test set predictions
+  accuracy(truth = pres, .pred_class)
+
+nb_testing_pred %>%                   # test set predictions
+  accuracy(truth = pres, .pred_class)
